d7a1668dccf79fa76bcf6a22dd7ee6d24a41c130
Rogues <rogues@es6.today>
2017-03-18 15:14:57 -0600

    [feat] Enable JavaScript Unit Testing.

---
+++ tests/index.spec.js
@@ -0,0 +1,9 @@
+import { expect } from 'chai';
+
+describe('thefrankenste.in', () => {
+  it('should be fun', () => {
+    const FUN = true;
+
+    expect(FUN).to.be.true; // eslint-disable-line no-unused-expressions
+  });
+});
